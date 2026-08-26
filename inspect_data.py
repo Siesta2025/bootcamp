0491ea5dee8112d119904080d86a2d73ec7ccde3
Siesta2025 <wangzl25@mails.tsinghua.edu.cn>
2026-08-26 18:16:57 +0800

    chore: finished dataloader demo

---
+++ inspect_data.py
@@ -0,0 +1,41 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+# 1.
+transform = transforms.ToTensor()
+
+# 2.
+dataset = datasets.CIFAR10(
+    root="./data",
+    train=True,
+    transform=transform,
+    download=True,
+)
+
+# 3.
+image = dataset[0][0]
+label = dataset[0][1]
+print(f"Type: {type(image)}")
+print(f"Image shape: {image.shape}")
+print(f"Image data type: {image.dtype}")
+print(f"Image min value: {image.min()}")
+print(f"Image max value: {image.max()}")
+print(f"Label: {label}")
+print(f"Label type: {type(label)}")
+
+# 4. 
+dataloader = DataLoader(
+    dataset, 
+    batch_size=8, 
+    shuffle=True, 
+    num_workers=0,
+)
+
+# 5. 
+images, labels = next(iter(dataloader))
+
+# 6. 
+print(f"Images shape: {images.shape}")
+print(f"Images data type: {images.dtype}")
+print(f"Labels shape: {labels.shape}")
+print(f"Labels data type: {labels.dtype}")
