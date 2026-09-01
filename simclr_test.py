03bedd6265755110e9683dcf3b125b79d6928e8d
Siesta2025 <wangzl25@mails.tsinghua.edu.cn>
2026-09-01 15:37:21 +0800

    build: write test scripts for simclr training; run complete simclr pretraining; implement encoder checkpoint reloading; primarily build linear probe components.

---
+++ simclr_test.py
@@ -0,0 +1,61 @@
+import torch
+
+from models import SmallResNet, ProjectionHead, SimCLRModel
+from simclr_train import NTXent
+from simclr_data import get_train_val_dataset, load_data
+
+# test1
+x = torch.randn(8, 3, 32, 32)
+encoder = SmallResNet()
+h = encoder(x)
+assert h.shape == (8, 256)
+projection_head = ProjectionHead(256, 128)
+z = projection_head(h)
+assert z.shape == (8, 128)
+
+# test2
+z1 = torch.randn(8, 128, requires_grad=True)
+z2 = torch.randn(8, 128, requires_grad=True)
+
+criterion = NTXent(tau=0.7)
+loss = criterion(z1, z2)
+assert loss.shape == torch.Size([]) # scalar value
+assert torch.isfinite(loss)
+assert loss.requires_grad
+loss.backward()
+
+# test3
+z1 = torch.randn(8, 128, requires_grad=True)
+z2 = torch.randn(8, 128, requires_grad=True)
+
+criterion = NTXent(tau=0.7)
+loss_random = criterion(z1, z2)
+
+z3 = torch.randn(8, 128, requires_grad=True)
+z4 = z3.clone()
+loss_same = criterion(z3, z4)
+
+assert loss_random > loss_same
+
+# test4
+dataset = get_train_val_dataset("./data")
+loader = load_data(dataset, batch_size=8)
+
+batch = next(iter(loader))
+(view1, view2), labels = batch
+
+assert view1.shape == (8, 3, 32, 32)
+assert view2.shape == view1.shape
+
+model = SimCLRModel(encoder, projection_head)
+z1 = model(view1)
+z2 = model(view2)
+assert z1.shape == (8, 128)
+assert z2.shape == z1.shape
+
+loss = criterion(z1, z2)
+assert loss.ndim == 0
+assert torch.isfinite(loss)
+assert loss.requires_grad
+
+loss.backward()
